c31089265a2d0265c6e838ef77090d56aa72ff7f
Akash-Verma96 <av3764909@gmail.com>
2026-07-13 16:04:27 +0530

    Added api versions

---
+++ src/routers/v2/index.router.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import pingRouter from '../ping.router';
+
+const v2Router = express.Router();
+
+v2Router.use("/", pingRouter);
+
+export default v2Router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import { serverConfig } from './config';
-import pingRouter from './routers/ping.router';
+import v1Router from './routers/v1/index.router';
+import v2Router from './routers/v2/index.router';
 
 const app = express();
 
-app.use("/", pingRouter); // Registering all the routes from pingRouter to the app
+app.use("/api/v1", v1Router); // Registering all the routes from pingRouter to the app
+app.use("/api/v2", v2Router); // Registering all the routes from pingRouter to the app
 
 
 app.listen(serverConfig.PORT, () => {
     console.log(`server is running on http://localhost:${serverConfig.PORT}`);
 });
+
